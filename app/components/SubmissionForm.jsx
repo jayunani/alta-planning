2844cf091b75a8d2c380ae576e4f2b70b4ad5c2e
jayunani <j.ayunani@gmail.com>
2016-10-09 14:20:42 -0700

    getting nodemailer to work

--- app/components/SubmissionForm.jsx
+++ app/components/SubmissionForm.jsx
@@ -1,22 +1,32 @@
 var React = require('react');
+var router = express.Router();
+var $ = require('jQuery');
+
 
 var SubmissionForm = React.createClass({
   onSubmit: function(e){
     e.preventDefault();
     var enteredName = this.refs.name.value;
-    var email = this.refs.email.value;
-    console.log("name " + enteredName);
-    console.log("email " + email);
-    this.refs.name.value = "";
-    this.refs.email.value = "";
+    var enteredEmail = this.refs.email.value;
+    $.ajax({
+      url: '/contactus',
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        console.log('success');
+      },
+      error: function(xhr, status, err) {
+        console.log(status);
+      }
+    });
   },
   render: function() {
     return (
       <div className="submission-form-box">
         <form action="mailto:j.ayunani@gmail.com" method="post" enctype="text/plain" ref="form" onSubmit={this.onSubmit} className="submission-form">
-          <input type="text" ref="name" placeholder="Name"/>
-          <input type="text" ref="email" placeholder="Email"/>
-          <button className="button expanded">Submit</button>
+          <input type="text" ref="name" name="name" placeholder="Name"/>
+          <input type="text" ref="email" name="email" placeholder="Email"/>
+          <button className="button expanded" onClick={this.onSubmit()}>Submit</button>
         </form>
       </div>
     )
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 
 var app = express();
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
+
+var nodemailer = require('nodemailer');
 
 app.use(function(req, res, next) {
     if(req.headers['x-forwarded-proto'] === 'https') {
@@ -13,6 +15,29 @@ app.use(function(req, res, next) {
 
 app.use(express.static('public'));
 
+app.post('/contactus', function(req, res){
+    var transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: "j.ayunani@gmail.com",
+            pass: "jayunani8521"
+        }
+    });
+    var mailContent = {
+        from: 'do-not-reply <j.ayunani@gmail.com>',
+        to: 'j.ayunani@gmail.com',
+        subject: 'form submission',
+        text: 'name: ' + req.body.name + 'email: ' + req.body.email;
+        html: <p>you have a new form submission: </p><ul><li>req.body.name</li></ul>
+    }
+    transporter.sendMail(mailContent, function(err, info) {
+        if(err){
+            console.log('error ' + error);
+        } else {
+            console.log('sent: ' + info.response);
+        }
+    })
+});
 
 app.listen(PORT, function() {
     console.log('Express server working on port ' + PORT);
